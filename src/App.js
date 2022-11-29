f992427d55a78583d91ac8d2974f7431408d4140
Heesoo LEe <heesoolee72@gmail.com>
2022-11-29 16:48:46 +0900

    wip

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>branch에서 내용을 수정함.</p>
+        <p>branch에서 내용을 수정함. 지속적인 배포 시도 중!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
