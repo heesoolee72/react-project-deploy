d621d6b0268e3690cdff58b1441a6761005790af
Heesoo LEe <heesoolee72@gmail.com>
2022-11-29 16:30:19 +0900

    내용 수정함

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>변경하기@@@@@!</p>
+        <p>branch에서 내용을 수정함.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
